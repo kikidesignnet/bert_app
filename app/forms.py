ff0c369d9e2b646a55e8e0c5f64daa3afc53d180
David Bressler <brainpainresearch@gmail.com>
2019-01-26 20:49:22 -0800

    s

--- app/forms.py
+++ app/forms.py
@@ -7,4 +7,4 @@ class QueryForm(FlaskForm):
     #the_document = StringField('Document', validators=[DataRequired()])
     the_wik_search = StringField('Search for a Wikipedia Page (e.g. "Janis Joplin")', default="Janis Joplin", validators=[DataRequired()])
     the_query = StringField('Your Question (e.g. "When was she born")', default="When was she born?", validators=[DataRequired()])
-    submit = SubmitField('Submit (15 sec wait)')
+    submit = SubmitField('Submit')
